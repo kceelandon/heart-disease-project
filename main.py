ec98b358d445720a50153b2f0ee3fd1933683c23
Kcee Landon <landonkcee@gmail.com>
2021-03-05 16:55:57 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import mean_squared_error
+
+def main():
+    data = pd.read_csv('cleveland.csv')
+
+if __name__ == '__main__':
+    main()
